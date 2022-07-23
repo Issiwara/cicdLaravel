7e460aeeaabbc148e6894b685a968d0a069de1d0
ijaz <ijaz013@gmail.com>
2022-07-23 14:56:43 +1000

    home file modified 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel CICD</title>
+        <h1>this is laravel CICD deployment</h1>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
